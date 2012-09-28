3bc58fd15af438636c210e868e24f7e04dbcfdf4
Boris Vujicic <turshija@gmail.com>
2012-09-28 13:53:23 +0200

    Added author to copyright info

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,7 @@
 /****************************************************************
  * KGB Admin extension for Chrome
  * Version 2.2 (28-SEP-2012)
+ * @author Boris Vujicic - turshija@gmail.com
  * Copyright (c) 2007-2012 KGB Hosting d.o.o.
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
